a4c30137fc2ea9d87caa9a0d737d3ca938b031a8
Matheus Avellar <matheusavellarb@gmail.com>
2015-10-08 20:22:02 -0300

    v0.2
    It's pretty now :smile:

--- ManagerThing/MainWindow.xaml.cs
+++ ManagerThing/MainWindow.xaml.cs
@@ -24,10 +24,44 @@ namespace ManagerThing
         public MainWindow()
         {
             InitializeComponent();
-            DispatcherTimer timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
-            {
-                this.dateText.Text = DateTime.Now.ToString("HH:mm:ss");
+            DispatcherTimer timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate {
+                this.timeText.Text = DateTime.Now.ToString("HH:mm:ss");
+                this.weekText.Text = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(DateTime.Now.ToString("dddd"));
+                this.dateText.Text = DateTime.Now.ToString("dd/MM/yyyy");
+                this.dateText.ToolTip = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(DateTime.Now.ToString("MMMM"));
             }, this.Dispatcher);
+        }
+
+        private void gridOnMouseEnter(object sender, MouseEventArgs e)
+        {
+            weekText.Foreground = dateText.Foreground = (Brush)new BrushConverter().ConvertFromString("#ffeeeeee");
+        }
+
+        private void gridOnMouseLeave(object sender, MouseEventArgs e)
+        {
+            weekText.Foreground = dateText.Foreground = (Brush)new BrushConverter().ConvertFromString("#ff444a59");
+        }
+
+
+
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void CloseWindow(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CloseWindowEnter(object sender, MouseEventArgs e)
+        {
+            closeWindowButton.Foreground = (Brush)new BrushConverter().ConvertFromString("#ffeeeeee");
+        }
+
+        private void CloseWindowLeave(object sender, MouseEventArgs e)
+        {
+            closeWindowButton.Foreground = (Brush)new BrushConverter().ConvertFromString("#ff1c1f25");
         }
     }
 }
