02d58d62fbade938cb895cdaba2ae61c93b49202
Matheus Avellar <matheusavellarb@gmail.com>
2015-10-08 21:57:32 -0300

    Updates! :smile:

--- ManagerThing/MainWindow.xaml.cs
+++ ManagerThing/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Net;
+using System.IO;
+using System.Net.Sockets;
 
 namespace ManagerThing
 {
@@ -24,11 +27,34 @@ namespace ManagerThing
         public MainWindow()
         {
             InitializeComponent();
+
+            int iterationCount = 0;
+
+            bool hasInternetConnection = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+            this.ipText.Text = (Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork)).ToString();
+
             DispatcherTimer timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate {
+                iterationCount++;
                 this.timeText.Text = DateTime.Now.ToString("HH:mm:ss");
                 this.weekText.Text = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(DateTime.Now.ToString("dddd"));
                 this.dateText.Text = DateTime.Now.ToString("dd/MM/yyyy");
                 this.dateText.ToolTip = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(DateTime.Now.ToString("MMMM"));
+
+                hasInternetConnection = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+
+                this.ipText_local.Text = (Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork)).ToString();
+                this.hasInternet.Text = (hasInternetConnection) ? "Has internet connection" : "No internet connection";
+
+                if (!hasInternetConnection) {
+                    this.hasInternet.Foreground = (Brush)new BrushConverter().ConvertFromString("#ffc42e3b");
+                    this.ipText.Text = this.ipText_local.Text;
+                } else {
+                    this.hasInternet.Foreground = (Brush)new BrushConverter().ConvertFromString("#ff90ad2f");
+                    if (this.ipText.Text == "127.0.0.1" || iterationCount == 5) {
+                        this.WindowTitle.Content = "Checking IP address...";
+                        this.ipText.Text = GetPublicIP().ToString();
+                    }
+                }
             }, this.Dispatcher);
         }
 
@@ -62,6 +88,30 @@ namespace ManagerThing
         private void CloseWindowLeave(object sender, MouseEventArgs e)
         {
             closeWindowButton.Foreground = (Brush)new BrushConverter().ConvertFromString("#ff1c1f25");
+        }
+
+        public string GetPublicIP()
+        {
+            try {
+                String direction = "";
+                WebRequest request = WebRequest.Create("http://matheus.avellar.c9.io/ip");
+                using (WebResponse response = request.GetResponse())
+                using (StreamReader stream = new StreamReader(response.GetResponseStream())) {
+                    direction = stream.ReadToEnd();
+                }
+                this.WindowTitle.Content = "Manager Thing";
+                return direction;
+            } catch {
+                this.WindowTitle.Content = "Manager Thing";
+                return "127.0.0.1";
+            }
+        }
+
+        private void IpRefresh(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowTitle.Content = "Checking IP address...";
+            this.ipText.Text = (Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork)).ToString();
+            this.ipText.Text = GetPublicIP().ToString();
         }
     }
 }
